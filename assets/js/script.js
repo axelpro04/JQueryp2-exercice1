ce1fa8c928e7da08e124337efdb5e7c576028c9c
axelpro04 <axellefpro@gmail.com>
2021-05-06 14:41:01 +0200

    exo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,23 @@
-$("#touch").mouseover(zoom);
-$("#touch").mouseout(originScale);
-    
-function zoom() {
-    $(this).css("width", "3000px");
-}
+$(document).ready(function () {
 
-function originScale() {
-    $(this).css("width", "200px");
-}
+    // $("#touch").mouseover(zoom);
+    // $("#touch").mouseout(originScale);
+
+    // function zoom() {
+    //     $(this).css("width", "500px");
+    // }
+
+    // function originScale() {
+    //     $("#touch").css("width", "200px");
+    // }
+
+    $("img").mouseover(function () {
+
+        $(this).css("width", "500px");
+
+    });
+    $("img").mouseout(function () {
+
+        $(this).css("width", "200px");
+    }); 
+});
